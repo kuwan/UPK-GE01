f664185711fef245e2c804f22c870c7f38af16d1
ZhaoJiaWei <JiaWei.Zhao@tcl.com>
2019-07-11 10:47:43 +0800

    创建项目

---
+++ UPK-GE01/UPK-GE01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UPK-GE01
+//
+//  Created by kuwan on 2019/7/11.
+//  Copyright © 2019 umehealltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
